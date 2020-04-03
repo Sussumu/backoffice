8dfbc1ff34041d9ea5ab200337daf70ca44f67ea
Gabriel Sussumu Kato <gkato@meuacesso.com.br>
2020-04-02 23:57:31 -0300

    Creates web api project with an endpoint to call query creator

---
+++ Backoffice.WebApi/Controllers/QueryWriterController.cs
@@ -0,0 +1,27 @@
+﻿using Backoffice.Application.Commands;
+using Backoffice.Application.Ports;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Backoffice.WebApi.Controllers
+{
+    [ApiController]
+    [Route("query")]
+    public class QueryWriterController : ControllerBase
+    {
+        public ICreateQuery QueryCreator { get; }
+
+        public QueryWriterController(ICreateQuery queryCreator)
+        {
+            QueryCreator = queryCreator ?? throw new System.ArgumentNullException(nameof(queryCreator));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateQueryCommand command)
+        {
+            await QueryCreator.Create(command);
+
+            return Ok();
+        }
+    }
+}
---
+++ Backoffice.WebApi/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Backoffice.WebApi
+{
+    public partial class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            ConfigureDependencyInjection();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
