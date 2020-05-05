824c364a9b4b6d0116ac3268b00cb2eab19eaaf2
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2020-05-04 22:15:36 -0300

    Change errors list from result object to keep the errors in one place

---
+++ Backoffice.Application/Contracts/Error.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Backoffice.Application.Contracts
+{
+    public class Error
+    {
+        public string Message { get; }
+
+        public Error(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentException("Message cannot be null", nameof(message));
+
+            Message = message;
+        }
+    }
+}
--- Backoffice.Application/Contracts/Result.cs
+++ Backoffice.Application/Contracts/Result.cs
@@ -6,30 +6,36 @@ namespace Backoffice.Application.Contracts
     public class Result
     {
         public bool Success { get; }
-        public object Data { get; }
-        public List<string> Errors { get; }
+        public List<Error> Errors { get; }
 
-        public static Result Ok()
+        public static Result Ok() => new Result(true);
+
+        public static Result Error(Error error) => new Result(false, errors: new List<Error> { error });
+
+        public static Result Error(IEnumerable<Error> errors) => new Result(false, errors);
+
+        private Result(bool success, IEnumerable<Error> errors = null)
         {
-            return new Result(true);
+            Success = success;
+            Errors = errors.ToList();
         }
+    }
 
-        public static Result Ok(object data)
-        {
-            return new Result(true, data);
-        }
+    public class Result<T> where T : class
+    {
+        public bool Success { get; }
+        public T Data { get; }
+        public List<Error> Errors { get; }
 
-        public static Result Error(object data)
-        {
-            return new Result(false, data);
-        }
+        public static Result<T> Ok(T data) => new Result<T>(true, data);
 
-        public static Result Error(object data, IEnumerable<string> errors)
-        {
-            return new Result(false, data, errors);
-        }
+        public static Result<T> Error(T data) => new Result<T>(false, data);
 
-        private Result(bool success, object data = default, IEnumerable<string> errors = null)
+        public static Result<T> Error(T data, Error error) => new Result<T>(false, data, new List<Error> { error });
+
+        public static Result<T> Error(T data, IEnumerable<Error> errors) => new Result<T>(false, data, errors);
+
+        private Result(bool success, T data, IEnumerable<Error> errors = null)
         {
             Success = success;
             Data = data;
