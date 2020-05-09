ebd916cebcda0ea25b8a4aac387bc93803224753
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2020-05-09 15:23:46 -0300

    Removes many configurations of the query param because it's not ideal for the user to keep track of database changes on their system and our backoffice at the same time. If the user requests something impossible, just let it throw an error.

--- 2-add-query-params-table.sql
+++ 2-add-query-params-table.sql
@@ -2,19 +2,9 @@
 
 sp_rename 'Query.Type', 'QueryType', 'column'
 
-create table ParamType (
-	Id tinyint not null primary key identity(1, 1),
-	Name varchar(50) not null	
-)
-
 create table QueryParam (
 	Id int not null primary key identity(1, 1),
 	Name varchar(100) not null,
 	Description varchar(200) null,
-	QueryId int not null foreign key references Query(Id),
-	ParamType tinyInt not null foreign key references ParamType(Id),
-	Required bit not null,
-	Length int null,
-	Precision int null,
-	Scale int null
+	QueryId int not null foreign key references Query(Id)
 )
---
+++ Backoffice.Application/Entities/QueryParamEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Backoffice.Application.Entities
+{
+    public class QueryParamEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int QueryId { get; set; }
+    }
+}
