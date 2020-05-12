0d515ce636281b45baa4a87f3d6afb2ad1304289
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2020-05-11 22:22:16 -0300

    Removes boilerplate and creates the queries list page and table

--- backoffice.frontend/src/App.vue
+++ backoffice.frontend/src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QueriesList></QueriesList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QueriesList from '../src/pages/QueriesList';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QueriesList: QueriesList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+}
+
+#app {  
+  height: 100vh;
+  background-color: #333;
+  color: white;
 }
 </style>
---
+++ backoffice.frontend/src/pages/QueriesList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Table></Table>
+    </div>
+</template>
+
+<script>
+import Table from '../components/Table';
+
+export default {
+    components: {
+        Table: Table
+    }
+}
+</script>
