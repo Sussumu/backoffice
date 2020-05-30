b3700d063646fee93c4cfd0e14eb43da2d7496da
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2020-05-29 23:04:21 -0300

    Creates a small skeleton of the form that will include new queries

--- backoffice.frontend/src/pages/QueriesList.vue
+++ backoffice.frontend/src/pages/QueriesList.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div id="queries-list-wrapper">
     <QueryTable :queries="queries"></QueryTable>
+    <form id="new-query-form">
+        <label>Name<input type="text" /></label>
+        <label>Description<input type="text" /></label>
+        <label>Query<textarea rows=4 columns=50 /></label>
+        <pre>TODO: Query params goes here!</pre>
+        <button>New Query</button>
+    </form>
   </div>
 </template>
 
@@ -22,3 +29,23 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+#queries-list-wrapper {
+    display: flex;
+}
+
+#new-query-form {
+    display: flex;
+    flex-direction: column;
+    margin-left: 40px;
+}
+
+#new-query-form > label {
+    margin-top: 20px;
+}
+
+#new-query-form > label > input {
+    width: 10%;
+}
+</style>
